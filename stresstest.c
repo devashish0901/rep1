c0363f4b4b63a1ac32381e893868e81735aca2c2
devashish <devashish.vij_ug24@ip-172-26-5-82.ap-south-1.compute.internal>
2022-10-26 16:47:58 +0000

    stresstest successfull with 15 0s

--- stresstest.c
+++ stresstest.c
@@ -4,11 +4,11 @@
 
     int main(int argc, char**argv){
         
-        float a, i=0.000000001;
-        float n= 0.00000001;
+        float a, i=0.00000000000000001;
+        float n= 0.0000000000000001;
         int c = 1;
 
-        for(i=0.0; a<n; a+=i){
+        for(a=0.0; a<n; a+=i){
             printf("Loop Number: %d\n", c);
             c++;
         }
