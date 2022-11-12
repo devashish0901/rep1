0365a91b1ba06e21769bb662b9037200078ce6f3
Devashish Vij <56601693+devashish0901@users.noreply.github.com>
2022-11-12 20:29:43 +0530

    addded more comments

--- linked_lists.c
+++ linked_lists.c
@@ -8,31 +8,31 @@
     typedef struct node Node; //making a node structure better abstraction and less typing.
 
     void printList(Node* head){
-        Node* temp = head;
-        while(temp != NULL){
-            printf((temp==head)?"%d":"->%d", temp->data);
-            temp = temp->linkNext;
+        Node* temp = head; //creates a temporary node and sets the value of the first node to it.
+        while(temp != NULL){ //loops till node reaches the end of the list. i.e, the last node has a NULL value.
+            printf((temp==head)?"%d":"->%d", temp->data); //prints the first node or the head node as it is and the rest with an arrow.
+            temp = temp->linkNext; //points to the next node to move forward in the linkedList. 
         }
-        printf("->NULL");
+        printf("->NULL"); //ends the list by pointing to the null value.
     }
 
     void appendLast(Node **headAddress, int input)
     {
-        Node *newNode = malloc(sizeof(Node));
-        newNode->data = input;
+        Node *newNode = malloc(sizeof(Node)); //allocate memory for a new node
+        newNode->data = input; //sets the data of the new node to the input by the user
         newNode->linkNext = NULL;
         if(*headAddress == NULL)
         {
             *headAddress= newNode; //sets the address to the new node, when the headAddress is null.
         }
-        else
+        else //executes if the the headAddress is not null, therefore there are already nodes in the list, therefore it finds the previous node
         {
-            Node *lastNode = *headAddress;
-            while(lastNode -> linkNext != NULL)
-            {
+            Node *lastNode = *headAddress; //points to the head address or the first node
+            while(lastNode -> linkNext != NULL) //checks for the lastNode since the last node will reference to the next Node which wil be null
+            { 
                 lastNode = lastNode -> linkNext;
             }
-            lastNode -> linkNext = newNode;
+            lastNode -> linkNext = newNode; //adds the new node ahead of the last node at the end of the list
 
         }
 
@@ -43,14 +43,14 @@
     //adding a function to add values to the created linked lists
     int main()
     {
-        Node *head= NULL;
+        Node *head= NULL; //creates a node head and sets it to null, this wil be the first node in the list
         
         int lengthList;
         int flag = 0;
         
 
         do{
-            printf("Input the length of the linked list below:");
+            printf("Input the length of the linked list below:"); //asks the user for an input for the list
             scanf("%d", &lengthList);
             if(lengthList<=0){
                 printf("Length of the list is invalid and should be positive!"); //invalid for negative input since length should be positive
@@ -75,7 +75,7 @@
             i++;
             }
 
-        printList(*head);
+        printList(head); //prints the linked list. 
 
         
         return 0;
