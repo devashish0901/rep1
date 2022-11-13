a088d33cd9eb99ba73c8cb6e9b11b0db37d47f87
Devashish Vij <56601693+devashish0901@users.noreply.github.com>
2022-11-13 19:37:10 +0530

    tweaked selsort to complete insertion sort

---
+++ insertion_sort.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include<time.h>
+
+
+int* GenerateRandom(int s)
+{
+    int* tempArr = (int*)malloc(s*sizeof(int));
+    for(int i=0; i<s; i++)
+    {
+        tempArr[i] = rand()%2000; //assigning random values to the array up to 1000 values
+    }
+    return tempArr;
+}
+
+int* UserInput(int n)
+{
+    int* arr = (int*)malloc(n*sizeof(int));
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter the value of the element %d", i+1);
+        printf(": ");
+        scanf("%d", &arr[i]);
+    }
+    return arr;
+
+}
+
+void sort(int *arrU, int size)
+{
+    int current;
+    int i, j;
+
+    for(i = 1 ; i<size; i++)
+    {
+        current= arrU[i]; //current is the value of the element we are looking at
+        j=i-1; //for it to move from 0, since i starts from 1st element
+        while(j>=0 && current<arrU[j]) //if the current value is less than the previous value
+        {
+            arrU[j+1] = arrU[j]; //value at index j+1 is replaced with the value at j
+            j--; //compares it to the elements before so the current value moves back to its corrected position
+        }
+        arrU[j+1] = current;    
+        
+    }
+}   
+
+
+
+void print(int *arrU, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        printf("%d", arrU[i]);
+        printf(" ");
+    }
+
+}
+
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+    int flag=0;
+    printf("Enter the size of the Array you want to input: \n");
+    int size; //size of the array
+    scanf("%d", &size);
+    do{
+        if(size<=0){
+            printf("Enter a valid size");
+            scanf("%d", &size);
+            flag=1;
+        }
+        else
+        {
+            flag=0;
+        }
+    }while(flag==1);
+    printf("\nEnter 1 for random array or 2 for user input array: \n");
+    int choice; //choice of the user    
+    scanf("%d", &choice);
+    do{
+        if(choice!=1 && choice!=2)
+        {
+            printf("Enter a valid choice");
+            scanf("%d", &choice);
+            flag=1;
+        }
+        else
+        {
+            flag=0;
+        }
+    }while(flag==1);
+    int *arrU; //array Unsorted
+
+    if(choice==1)
+    {
+        arrU = GenerateRandom(size);
+    }
+    else
+    {
+        arrU = UserInput(size);
+    }
+
+    printf("\nYour array is:\n ");
+    print(arrU, size);
+    sort(arrU, size);
+    printf("\nYour sorted array is:\n ");
+    print(arrU, size);
+    return 0;
+
+
+}
