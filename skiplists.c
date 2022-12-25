98ebe9afa0e8032de026defda55e573d347ade3c
Devashish Vij <56601693+devashish0901@users.noreply.github.com>
2022-12-25 19:46:29 +0530

    trying to  implementing skiplists

---
+++ skiplists.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//program to implement skip lists in C 
+
+//function to create a new node
+struct node *createNode(int val, int level)
+{
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    new->val = val;
+    new->forward = (struct node **)malloc((level + 1) * sizeof(struct node *));
+    return new;
+}
+
+//function to create a new skip list
+
+struct skipList *createSkipList(int maxLevel, float prob)
+{
+    struct skipList *new = (struct skipList *)malloc(sizeof(struct skipList));
+    new->maxLevel = maxLevel;
+    new->prob = prob;
+    new->header = createNode(-1, maxLevel);
+    return new;
+}
+
+
+//function to create a random level for the node
+
+int randomLevel(struct skipList *sl)
+{
+    int lvl = 0;
+    while (rand() < sl->prob * RAND_MAX && lvl < sl->maxLevel)
+        lvl++;
+    return lvl;
+}
+
+//function to insert a node in the skip list
